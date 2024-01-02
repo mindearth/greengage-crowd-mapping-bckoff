dfd01c7b99a8834a9fcabd4d5af6bb96523a9f1e
filippo <filippo.cazzarolli@mindearth.ch>
2024-01-02 15:46:45 +0100

    wip

--- src/mission/MissionList.jsx
+++ src/mission/MissionList.jsx
@@ -1,10 +1,10 @@
-import {Button, Radio, Result, Select, Space} from "antd";
+import {Button, Radio, Select, Space, Table} from "antd";
 import {ReloadOutlined} from "@ant-design/icons";
 import {useEffect, useState} from "react";
-import {MissionDraw} from "./MissionDraw.jsx";
 import {listCampaign} from "../campaign/CampaignService.js";
 import {useAuth} from "react-oidc-context";
 import Search from "antd/lib/input/Search.js";
+import {listMission} from "./MissionService.js";
 
 export function MissionList() {
     const auth = useAuth();
@@ -21,15 +21,78 @@ export function MissionList() {
     const [isLoading, setIsLoading] = useState(true);
     const [campaignIdxSelected, setCampaignIdxSelected] = useState(null);
     const [campaignData, setCampaignData] = useState([]);
-    const [modalMissionIsOpen, setModalMissionIsOpen] = useState(false);
-
-    function drawNewMission() {
-        if (campaignIdxSelected !== null) {
-            setModalMissionIsOpen(true)
+    const [data, setData] = useState([]);
+    const [filterText, setFilterText] = useState('');
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+            defaultSortOrder: 'descend',
+            sorter: (a, b) => a.name < b.name,
+            sortDirections: ['descend', 'ascend'],
+        },
+        {
+            title: 'Status',
+            width: '140px',
+            dataIndex: 'status',
+            key: 'status',
+        },
+        {
+            title: 'Kind',
+            width: '140px',
+            dataIndex: 'kind',
+            key: 'kind',
+        },
+        {
+            title: 'Distance mt',
+            width: '140px',
+            dataIndex: 'distanceMt',
+            key: 'distanceMt',
+        },
+        {
+            title: 'Duration min',
+            width: '140px',
+            dataIndex: 'durationMin',
+            key: 'durationMin',
+        },
+        {
+            title: 'Reward',
+            width: '140px',
+            dataIndex: 'reward',
+            key: 'reward',
         }
+    ]
+
+
+    async function getData() {
+        setIsLoading(true)
+        setData([])
+
+        const response = await listMission(auth.user.access_token);
+
+        setData(response.data)
+        setIsLoading(false)
+    }
+
+    function onSearch(value) {
+        setFilterText(value.toUpperCase())
+    }
+
+    function filterData() {
+        const result = []
+
+        data.forEach(item => {
+            if (item.name.toUpperCase().includes(filterText)) {
+                result.push(item)
+            }
+        });
+
+        return result
     }
 
     useEffect(() => {
+        getData()
 
         listCampaign(auth.user.access_token).then(response => {
             setCampaignData(response.data)
@@ -48,6 +111,7 @@ export function MissionList() {
             }}>
                 <Space>
                     <Button
+                        onClick={getData}
                         type="primary" icon={<ReloadOutlined/>}></Button>
                     <Select
                         placeholder="Select a campaign"
@@ -61,6 +125,7 @@ export function MissionList() {
                         })}
                     />
                     <Search
+                        onSearch={onSearch}
                         placeholder="Filter"
                         style={{
                             width: 200,
@@ -70,23 +135,15 @@ export function MissionList() {
                     <Radio.Group options={MissionOptions} optionType="button" buttonStyle="solid"/>
                 </Space>
 
-                <Button
-                    onClick={drawNewMission}
-                    disabled={campaignIdxSelected === null}
-                    type="primary">Draw new mission</Button>
+                <div></div>
             </div>
 
-            {campaignIdxSelected === null && <Result
-                status="warning"
-                title="Select a campaign before show data."
-            />}
-
-            <MissionDraw
-                campaignId={campaignIdxSelected !== null ? campaignData[campaignIdxSelected].id : null}
-                campaignName={campaignIdxSelected !== null ? campaignData[campaignIdxSelected].name : null}
-                campaignGeoJson={campaignIdxSelected !== null ? campaignData[campaignIdxSelected].geojson : null}
-                modalMissionIsOpen={modalMissionIsOpen}
-                setModalMissionIsOpen={setModalMissionIsOpen}/>
+            <Table
+                rowKey={record => record.id}
+                columns={columns}
+                dataSource={filterText === "" ? data : filterData()}
+                loading={isLoading}
+            />
         </>
     )
 }
